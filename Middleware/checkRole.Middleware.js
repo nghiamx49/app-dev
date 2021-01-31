39c3be8cc71e6b33ebfa0be48da54b1264ce4bd5
nghiamx49 <mxnghia49@gmail.com>
2021-01-31 22:44:24 +0700

    fix JSON response object

--- Middleware/checkRole.Middleware.js
+++ Middleware/checkRole.Middleware.js
@@ -4,99 +4,103 @@ const Role = db.roles;
 const passport = require("passport");
 const passportConf = require("./Auth.Middleware");
 
-const isAdmin = async (req, res, next) => {
-  try {
-    const { _id } = req.user;
-    const findUser = await User.findById(_id);
-    const findRole = await Role.findById(findUser.roleId[0]);
-    const { name } = findRole;
-    if (name !== "admin") {
-      res.status(403).json({
-        message: { mesBody: "Your dont have permission to access this page" },
-        mesErorr: true,
-      });
+const isAdmin = async(req, res, next) => {
+    try {
+        const { _id } = req.user;
+        const findUser = await User.findById(_id);
+        const findRole = await Role.findById(findUser.roleId[0]);
+        const { name } = findRole;
+        if (name !== "admin") {
+            res.status(403).json({
+                message: { mesBody: "Your dont have permission to access this page" },
+                mesErorr: true,
+            });
+            return;
+        }
+        next();
+    } catch (error) {
+        res.status(500).json({
+            message: { mesBody: "Errors" },
+            mesErorr: true,
+        });
+        next(error);
     }
-    next();
-  } catch (error) {
-    res.status(500).json({
-      message: { mesBody: "Errors" },
-      mesErorr: true,
-    });
-    next(error);
-  }
 };
 
-const isStaff = async (req, res, next) => {
-  try {
-    const { _id } = req.user;
-    const findUser = await User.findById(_id);
-    const findRole = await Role.findById(findUser.roleId[0]);
-    const { name } = findRole;
-    if (name !== "staff") {
-      res.status(403).json({
-        message: { mesBody: "Your dont have permission to access this page" },
-        mesErorr: true,
-      });
+const isStaff = async(req, res, next) => {
+    try {
+        const { _id } = req.user;
+        const findUser = await User.findById(_id);
+        const findRole = await Role.findById(findUser.roleId[0]);
+        const { name } = findRole;
+        if (name !== "staff") {
+            res.status(403).json({
+                message: { mesBody: "Your dont have permission to access this page" },
+                mesErorr: true,
+            });
+            return;
+        }
+        next();
+    } catch (error) {
+        res.status(500).json({
+            message: { mesBody: "Errors" },
+            mesErorr: true,
+        });
+        next(error);
     }
-    next();
-  } catch (error) {
-    res.status(500).json({
-      message: { mesBody: "Errors" },
-      mesErorr: true,
-    });
-    next(error);
-  }
 };
 
-const isTrainer = async (req, res, next) => {
-  try {
-    const { _id } = req.user;
-    const findUser = await User.findById(_id);
-    const findRole = await Role.findById(findUser.roleId[0]);
-    const { name } = findRole;
-    if (name !== "trainer") {
-      res.status(403).json({
-        message: { mesBody: "Your dont have permission to access this page" },
-        mesErorr: true,
-      });
+const isTrainer = async(req, res, next) => {
+    try {
+        const { _id } = req.user;
+        const findUser = await User.findById(_id);
+        const findRole = await Role.findById(findUser.roleId[0]);
+        const { name } = findRole;
+        if (name !== "trainer") {
+            res.status(403).json({
+                message: { mesBody: "Your dont have permission to access this page" },
+                mesErorr: true,
+            });
+            return;
+        }
+        next();
+    } catch (error) {
+        res.status(500).json({
+            message: { mesBody: "Errors" },
+            mesErorr: true,
+        });
+        next(error);
     }
-    next();
-  } catch (error) {
-    res.status(500).json({
-      message: { mesBody: "Errors" },
-      mesErorr: true,
-    });
-    next(error);
-  }
 };
 
-const isTrainee = async (req, res, next) => {
-  try {
-    const { _id } = req.user;
-    const findUser = await User.findById(_id);
-    const findRole = await Role.findById(findUser.roleId[0]);
-    const { name } = findRole;
-    if (name !== "trainee") {
-      res.status(403).json({
-        message: { mesBody: "Your dont have permission to access this page" },
-        mesErorr: true,
-      });
+const isTrainee = async(req, res, next) => {
+    try {
+        const { _id } = req.user;
+        const findUser = await User.findById(_id);
+        const findRole = await Role.findById(findUser.roleId[0]);
+        const { name } = findRole;
+        if (name !== "trainee") {
+            res.status(403).json({
+                message: { mesBody: "Your dont have permission to access this page" },
+                mesErorr: true,
+            });
+            return;
+        }
+        next();
+    } catch (error) {
+        res.status(500).json({
+            message: { mesBody: "Errors" },
+            mesErorr: true,
+        });
+        next(error);
     }
-    next();
-  } catch (error) {
-    res.status(500).json({
-      message: { mesBody: "Errors" },
-      mesErorr: true,
-    });
-    next(error);
-  }
 };
 
 const checkRole = {
-  isAdmin,
-  isStaff,
-  isTrainer,
-  isTrainee,
+    isAdmin,
+    isStaff,
+    isTrainer,
+    isTrainee,
 };
 
 module.exports = checkRole;
--- apiRoute/users/user.RelatedCourses.js
+++ apiRoute/users/user.RelatedCourses.js
@@ -5,161 +5,172 @@ const Course = db.courses;
 const User = db.users;
 const RelatedCourses = db.relatedCourses;
 const Role = db.roles;
+const Category = db.categories;
 
 //get all courses that related to the user, allow all roles
-userRelatedCourses.get("/", async (req, res, next) => {
-  try {
-    let allRelatedCourses = await RelatedCourses.find({
-      userId: req.params.userId,
-    });
-    if (!allRelatedCourses.length) {
-      res
-        .status(404)
-        .json({ message: { mesBody: "No related courses" }, mesError: true });
+userRelatedCourses.get("/", async(req, res, next) => {
+    try {
+        let allRelatedCourses = await RelatedCourses.find({
+            userId: req.params.userId,
+        });
+        if (!allRelatedCourses.length) {
+            res
+                .status(404)
+                .json({ message: { mesBody: "No related courses" }, mesError: true });
+        }
+        const relatedCourses = await Promise.all(
+            allRelatedCourses.map(async(relatedCourse) => {
+                const { _id, courseId, userId } = relatedCourse;
+                const user = await User.findById(userId[0]);
+                const course = await Course.findById(courseId[0]);
+                const category = await Category.findById(course.categoryId[0]);
+                let obj = {
+                    _id,
+                    userId,
+                    courseId,
+                    username: user.username || "",
+                    courseName: course.name || "",
+                    categoryName: category.name || "",
+                };
+                return obj;
+            })
+        );
+        res
+            .status(200)
+            .json({ message: { relatedCourses: relatedCourses }, mesError: false });
+    } catch (error) {
+        res.status(500).json({ message: { mesBody: "Errors" }, mesError: true });
+        next(error);
     }
-    const relatedCourses = await Promise.all(
-      allRelatedCourses.map(async (relatedCourse) => {
-        const { _id, courseId, userId } = relatedCourse;
-        const user = await User.findById(userId[0]);
-        const course = await Course.findById(courseId[0]);
-        let obj = {
-          _id,
-          userId,
-          courseId,
-          username: user.username || "",
-          course: course.name || "",
-        };
-        return obj;
-      })
-    );
-    res
-      .status(200)
-      .json({ message: { relatedCourses: relatedCourses }, mesError: false });
-  } catch (error) {
-    res.status(500).json({ message: { mesBody: "Errors" }, mesError: true });
-    next(error);
-  }
 });
 //return data for select options
 userRelatedCourses.get(
-  "/dataoptional",
-  checkRole.isStaff,
-  async (req, res, next) => {
-    try {
-      let allCourses = await Course.find({});
-      let allUser = await Course.find({});
-      res.status(200).json({
-        message: {
-          mesBody: { courses: allCourses, users: allUser },
-        },
-        mesError: true,
-      });
-    } catch (error) {
-      res.status(500).json({ message: { mesBody: "Errors" }, mesError: true });
-      next(error);
+    "/dataoptional",
+    checkRole.isStaff,
+    async(req, res, next) => {
+        try {
+            let allCourses = await Course.find({});
+            let allUser = await Course.find({});
+            res.status(200).json({
+                message: {
+                    mesBody: { courses: allCourses, users: allUser },
+                },
+                mesError: true,
+            });
+        } catch (error) {
+            res.status(500).json({ message: { mesBody: "Errors" }, mesError: true });
+            next(error);
+        }
     }
-  }
 );
 //assgin courses to trainers
 userRelatedCourses.post(
-  "/assign",
-  checkRole.isStaff,
-  async (req, res, next) => {
-    try {
-      const { username, course } = req.body;
-      let courseId = await Course.findOne({ name: course });
-      let userId = await User.findOne({ username: username });
-      let checkUserRole = await Role.findById(userId.roleId);
-      if (checkUserRole.name === "admin" || checkUserRole.name === "staff") {
-        res.status(400).json({
-          message: { mesBody: "Cannot assign course to admin/staff account" },
-          mesError: true,
-        });
-      }
-      let relatedCourse = await new RelatedCourses({
-        courseId,
-        userId,
-      });
-      await relatedCourse.save();
-      res.status(200).json({
-        message: { mesBody: "Assign course successfully" },
-        mesError: false,
-      });
-    } catch (error) {
-      res.status(500).json({ message: { mesBody: "Errors" }, mesError: true });
-      next(error);
+    "/assign",
+    checkRole.isStaff,
+    async(req, res, next) => {
+        try {
+            const { username, course } = req.body;
+            let courseId = await Course.findOne({ name: course });
+            let userId = await User.findOne({ username: username });
+            let checkUserRole = await Role.findById(userId.roleId);
+            if (checkUserRole.name === "admin" || checkUserRole.name === "staff") {
+                res.status(400).json({
+                    message: { mesBody: "Cannot assign course to admin/staff account" },
+                    mesError: true,
+                });
+            }
+            let relatedCourse = await new RelatedCourses({
+                courseId,
+                userId,
+            });
+            await relatedCourse.save();
+            res.status(200).json({
+                message: { mesBody: "Assign course successfully" },
+                mesError: false,
+            });
+        } catch (error) {
+            res.status(500).json({ message: { mesBody: "Errors" }, mesError: true });
+            next(error);
+        }
     }
-  }
 );
 
 userRelatedCourses.param(
-  "relatedCourseId",
-  async (req, res, next, relatedCourseId) => {
-    try {
-      let relatedCourse = await RelatedCourses.findById(relatedCourseId);
-      if (!relatedCourse) {
-        res.status(404).json({
-          message: { mesBody: "Related course not found" },
-          mesError: true,
-        });
-      }
-      const { _id, userId, courseId } = relatedCourse;
-      let course = await Course.findById(courseId[0]);
-      let user = await User.findById(userId[0]);
-      req.relatedCourse = {
-        _id,
-        userId,
-        username: user.username || "",
-        courseId,
-        course: course.name || "",
-      };
-      next();
-    } catch (error) {
-      res.status(500).json({ message: { mesBody: "Errors" }, mesError: true });
-      next(error);
+    "relatedCourseId",
+    async(req, res, next, relatedCourseId) => {
+        try {
+            let relatedCourse = await RelatedCourses.findById(relatedCourseId);
+            if (!relatedCourse) {
+                res.status(404).json({
+                    message: { mesBody: "Related course not found" },
+                    mesError: true,
+                });
+            }
+            const { _id, userId, courseId } = relatedCourse;
+            let course = await Course.findById(courseId[0]);
+            let category = await Category.findById(course.categoryId[0]);
+            let user = await User.findById(userId[0]);
+            req.relatedCourse = {
+                _id,
+                userId,
+                username: user.username || "",
+                courseId,
+                courseName: course.name || "",
+                courseDescription: course.description || "",
+                categoryName: category.name || "",
+            };
+            next();
+        } catch (error) {
+            res.status(500).json({ message: { mesBody: "Errors" }, mesError: true });
+            next(error);
+        }
     }
-  }
 );
 
 userRelatedCourses.get("/detail/:relatedCourseId", (req, res, next) => {
-  res
-    .status(200)
-    .json({ message: { relatedCourse: req.relatedCourse }, mesError: false });
+    res
+        .status(200)
+        .json({ message: { relatedCourse: req.relatedCourse }, mesError: false });
 });
 //change assigned course to another course
-userRelatedCourses.put("/change/:relatedCourseId", async (req, res, next) => {
-  try {
-    const { _id } = req.relatedCourse;
-    const { course } = req.body;
-    let relatedCourse = await RelatedCourses.findById(_id);
-    let findCourse = await Course.find({ name: course });
-    relatedCourse.courseId = findCourse;
-    await relatedCourse.save();
-    res.status(200).json({
-      message: { mesBody: "Change assigned course to user successfully" },
-      mesError: false,
-    });
-  } catch (error) {
-    res.status(500).json({ message: { mesBody: "Errors" }, mesError: true });
-    next(error);
-  }
-});
+userRelatedCourses.put(
+    "/change/:relatedCourseId",
+    checkRole.isStaff,
+    async(req, res, next) => {
+        try {
+            const { _id } = req.relatedCourse;
+            const { course } = req.body;
+            let relatedCourse = await RelatedCourses.findById(_id);
+            let findCourse = await Course.find({ name: course });
+            relatedCourse.courseId = findCourse;
+            await relatedCourse.save();
+            res.status(200).json({
+                message: { mesBody: "Change assigned course to user successfully" },
+                mesError: false,
+            });
+        } catch (error) {
+            res.status(500).json({ message: { mesBody: "Errors" }, mesError: true });
+            next(error);
+        }
+    }
+);
 
 userRelatedCourses.delete(
-  "/remove/:relatedCourseId",
-  async (req, res, next) => {
-    const { _id } = req.relatedCourse;
-    try {
-      await RelatedCourses.deleteOne({ _id });
-      res.status(200).json({
-        message: { mesBody: "Remove assigned course from user successfully" },
-        mesError: false,
-      });
-    } catch (error) {
-      res.status(500).json({ message: { mesBody: "Errors" }, mesError: true });
-      next(error);
+    "/remove/:relatedCourseId",
+    checkRole.isStaff,
+    async(req, res, next) => {
+        const { _id } = req.relatedCourse;
+        try {
+            await RelatedCourses.deleteOne({ _id });
+            res.status(200).json({
+                message: { mesBody: "Remove assigned course from user successfully" },
+                mesError: false,
+            });
+        } catch (error) {
+            res.status(500).json({ message: { mesBody: "Errors" }, mesError: true });
+            next(error);
+        }
     }
-  }
 );
 
 module.exports = userRelatedCourses;
