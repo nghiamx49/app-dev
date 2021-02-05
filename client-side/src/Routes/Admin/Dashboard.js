fceb8d9959a160d55359aace7c37d0ca1750451c
nghiamx49 <mxnghia49@gmail.com>
2021-02-05 21:02:41 +0700

    push for production

--- client-side/src/Routes/Admin/Dashboard.js
+++ client-side/src/Routes/Admin/Dashboard.js
@@ -1,22 +1,65 @@
 import React, { useContext, useState, useEffect } from "react";
 import NavBar from "../Components/Navbar";
-import ContentAsTable from "../Components/ContentAsTable";
 import { AuthContext } from "../../Context/Auth.Context";
+import SystemInfo from "../../Services/SystemInfo.Service";
 import Footer from "../Components/Footer";
+import { Link } from "react-router-dom";
+import { Row, Col, Card, Container } from "react-bootstrap";
 
 const Dashboard = () => {
   const { user } = useContext(AuthContext);
 
+  const [systemInfo, setSystemInfo] = useState({});
   const [role, setRole] = useState(null);
 
+  const getSystemInfo = async () => {
+    let data = await SystemInfo.forAdmin();
+    setSystemInfo(data.message);
+  };
+
   useEffect(() => {
+    getSystemInfo();
     setRole(user.role);
   }, [user.role]);
 
   return (
     <>
       <NavBar role={role} />
-      <ContentAsTable list={[]} role={{}} />
+      <Container>
+        <div>
+          <h1 className="text-center">Application Status</h1>
+          <Row>
+            <Col style={{ margin: "50px" }}>
+              <Card style={{ width: "18rem" }}>
+                <Card.Body>
+                  <Card.Title>Trainer</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted"></Card.Subtitle>
+                  <Card.Text>
+                    Number of Trainer in system:{systemInfo.trainer};
+                  </Card.Text>
+                  <Link to="/admin/trainer">
+                    <div className="btn btn-primary">View All Trainer</div>
+                  </Link>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col style={{ margin: "50px" }}>
+              <Card style={{ width: "18rem" }}>
+                <Card.Body>
+                  <Card.Title>Staff</Card.Title>
+                  <Card.Subtitle className="mb-2 text-muted"></Card.Subtitle>
+                  <Card.Text>
+                    Number of Staff in system:{systemInfo.staff};
+                  </Card.Text>
+                  <Link to="/admin/staff">
+                    <div className="btn btn-primary">View All Staff</div>
+                  </Link>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      </Container>
       <Footer />
     </>
   );
--- client-side/src/Routes/User/Home.js
+++ client-side/src/Routes/User/Home.js
@@ -1,13 +1,153 @@
 import React, { useContext, useState, useEffect } from "react";
 import NavBar from "../Components/Navbar";
-import ContentAsTable from "../Components/ContentAsTable";
 import { AuthContext } from "../../Context/Auth.Context";
 import Footer from "../Components/Footer";
+import { Row, Col, Card, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import SystemInfo from "../../Services/SystemInfo.Service";
 
 const Dashboard = () => {
   const { user } = useContext(AuthContext);
+  const [systemInfo, setSystemInfo] = useState({});
 
   const [role, setRole] = useState(null);
+
+  const getSystemInfo = async () => {
+    if (user.role === "staff") {
+      let data = await SystemInfo.forStaff();
+      setSystemInfo(data.message);
+    } else if (user.role === "trainee" || user.role === "trainer") {
+      let data = SystemInfo.forUser(user._id);
+      setSystemInfo(data.message);
+    } else {
+      return;
+    }
+  };
+
+  const body = () => {
+    if (user.role === "staff") {
+      return (
+        <>
+          <Container>
+            <div>
+              <h1 className="text-center">Application Status</h1>
+              <Row style={{ marginBottom: "50px" }}>
+                <Col style={{ margin: "50px" }}>
+                  <Card style={{ width: "18rem" }}>
+                    <Card.Body>
+                      <Card.Title>Trainer</Card.Title>
+                      <Card.Subtitle className="mb-2 text-muted"></Card.Subtitle>
+                      <Card.Text>
+                        Number of Trainer in system:{systemInfo.trainers};
+                      </Card.Text>
+                      <Link to="/home/trainer">
+                        <div className="btn btn-primary">View All Trainer</div>
+                      </Link>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col style={{ margin: "50px" }}>
+                  <Card style={{ width: "18rem" }}>
+                    <Card.Body>
+                      <Card.Title>Trainee</Card.Title>
+                      <Card.Subtitle className="mb-2 text-muted"></Card.Subtitle>
+                      <Card.Text>
+                        Number of Trainee in system:{systemInfo.trainees};
+                      </Card.Text>
+                      <Link to="/home/trainee">
+                        <div className="btn btn-primary">View All Trainee</div>
+                      </Link>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </Row>
+
+              <Row>
+                <Col style={{ margin: "50px" }}>
+                  <Card style={{ width: "18rem" }}>
+                    <Card.Body>
+                      <Card.Title>Courses</Card.Title>
+                      <Card.Subtitle className="mb-2 text-muted"></Card.Subtitle>
+                      <Card.Text>
+                        Number of Courses in system:{systemInfo.courses};
+                      </Card.Text>
+                      <Link to="/home/courses">
+                        <div className="btn btn-primary">View All Courses</div>
+                      </Link>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col style={{ margin: "50px" }}>
+                  <Card style={{ width: "18rem" }}>
+                    <Card.Body>
+                      <Card.Title>Categories</Card.Title>
+                      <Card.Subtitle className="mb-2 text-muted"></Card.Subtitle>
+                      <Card.Text>
+                        Number of Categories in system:{systemInfo.categories};
+                      </Card.Text>
+                      <Link to="/home/categories">
+                        <div className="btn btn-primary">
+                          View All Categories
+                        </div>
+                      </Link>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </Row>
+            </div>
+          </Container>
+        </>
+      );
+    } else if (user.role === "trainee" || user.role === "trainer") {
+      return (
+        <>
+          <Container>
+            <div>
+              <Row>
+                <Col style={{ margin: "50px" }}>
+                  <Card style={{ width: "18rem" }}>
+                    <Card.Body>
+                      <Card.Title>Courses</Card.Title>
+                      <Card.Subtitle className="mb-2 text-muted"></Card.Subtitle>
+                      <Card.Text>
+                        Number of Courses in system:{systemInfo.courses};
+                      </Card.Text>
+                      {user.role === "trainee" && (
+                        <Link to="/home/courses">
+                          <div className="btn btn-primary">
+                            View All Courses
+                          </div>
+                        </Link>
+                      )}
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col style={{ margin: "50px" }}>
+                  <Card style={{ width: "18rem" }}>
+                    <Card.Body>
+                      <Card.Title>Related Courses</Card.Title>
+                      <Card.Subtitle className="mb-2 text-muted"></Card.Subtitle>
+                      <Card.Text>
+                        You have <strong>{systemInfo.relatedCourses}</strong>{" "}
+                        related;
+                      </Card.Text>
+                      <Link to="/home/relatedcourses">
+                        <div className="btn btn-primary">
+                          View All Related Courses
+                        </div>
+                      </Link>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </Row>
+            </div>
+          </Container>
+        </>
+      );
+    } else {
+      return;
+    }
+  };
 
   useEffect(() => {
     setRole(user.role);
@@ -16,7 +156,7 @@ const Dashboard = () => {
   return (
     <>
       <NavBar role={role} />
-      <ContentAsTable />
+      {body()}
       <Footer />
     </>
   );
